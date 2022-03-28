32e069d5fb9cfd30aa70eb667405360fa6140dba
MarahDaher <marah.daher193@outlook.com>
2022-03-28 15:10:33 +0300

    Add loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, Injector, OnInit } from '@angular/core';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterEvent } from '@angular/router';
 import { BaseComponent } from './shared/components/base.component';
 
 
@@ -10,10 +11,42 @@ import { BaseComponent } from './shared/components/base.component';
 })
 export class AppComponent extends BaseComponent implements OnInit{
 
+  loading = true;
+
   constructor(
-    injector: Injector
-  ){
+    injector: Injector,
+    private router: Router
+    ) {
     super(injector);
+    router.events.subscribe((event: any) => {
+      this.navigationInterceptor(event);
+    });
   }
-  ngOnInit() { }
-}  
+
+  ngOnInit(): void {
+  }
+
+  // Shows and hides the loading spinner during RouterEvent changes
+  navigationInterceptor(event: RouterEvent): void {
+    if (event instanceof NavigationStart) {
+      this.loading = true;
+    }
+    if (event instanceof NavigationEnd) {
+      setTimeout(() => { // here
+        this.loading = false;
+      }, 2000);
+    }
+
+    // Set loading state to false in both of the below events to hide the spinner in case a request fails
+    if (event instanceof NavigationCancel) {
+      setTimeout(() => { // here
+        this.loading = false;
+      }, 2000);
+    }
+    if (event instanceof NavigationError) {
+      setTimeout(() => { // here
+        this.loading = false;
+      }, 2000);
+    }
+  }
+}
